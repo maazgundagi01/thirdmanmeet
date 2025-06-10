223ba5c024e5fea46f28c52c4250de0728abace2
Maaz Gundagi <maazyg.01@gmail.com>
2025-06-09 23:15:25 -0500

    database setup - drizzle, postgres with neon, added push and studio scripts

--- thirdmanmeet/src/app/page.tsx
+++ thirdmanmeet/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <div className="text-4xl font-bold text-green-500">Hello World </div> 
+   <Button>
+    Hello Button
+   </Button>
   );
 }
